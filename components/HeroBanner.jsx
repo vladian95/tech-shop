06ddcd9abd227db9d4941b6b5854aeaffbb02890
vladian95 <kvankvan1995@gmail.com>
2023-04-08 13:49:20 +0300

    ad

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+// import imgBanner from '../public/assets/mainBanner';
 
 const HeroBanner = () => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">Small Text</p>
-        <h3>MID TEXT</h3>
+        <p className="beats-solo">Beats Solo Air</p>
+        <h3>Summer Sale</h3>
         <img src="" alt="headphones" className="hero-banner-image" />
         <div>
           <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+            <button type="button">Check It</button>
           </Link>
           <div className="desc">
-            <h5>description</h5>
-            <p>DESCRIPTION</p>
+            <h5>Description</h5>
+            <p>Best headphones on the market</p>
           </div>
         </div>
       </div>
